835e76d72ca6b0e2344d6a5c4157e1820746a36c
vinytec <vinytec@vinytec-VirtualBox>
2020-02-19 12:31:57 -0300

    projeto final

--- src/course/java/enums/entities/Order.java
+++ src/course/java/enums/entities/Order.java
@@ -11,11 +11,46 @@ public class Order {
 	private OrderStatus status;
 
 	public Order(Integer id, Date moment, OrderStatus status) {
-		super();
 		this.id = id;
 		this.moment = moment;
 		this.status = status;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+
+	public Date getMoment() {
+		return moment;
+	}
+
+
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+
+
 
 	@Override
 	public String toString() {
